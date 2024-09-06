f3d184bd4ca57da8c82a3f89dae455a6dd96700d
Brian Macomber <brian@agspectrum.com>
2024-09-06 16:11:22 -0500

    wip

--- SfListViewTemplateSelector/MainPage.xaml.cs
+++ SfListViewTemplateSelector/MainPage.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using Syncfusion.Maui.DataSource;
+using Syncfusion.Maui.DataSource.Extensions;
 
 namespace SfListViewTemplateSelector
 {
@@ -10,14 +11,16 @@ namespace SfListViewTemplateSelector
 
 			BindingContext = new ViewModel();
 
-			//ListView.DataSource.GroupDescriptors.Add(new GroupDescriptor()
-			//{
-			//	PropertyName = "Type",
-			//	KeySelector = o =>
-			//	{
-			//		return "Group";
-			//	}
-			//});
+			ListView.DataSource.GroupDescriptors.Add(new GroupDescriptor()
+			{
+				PropertyName = "Group",
+				KeySelector = o =>
+				{
+					if (o is DataItem item)
+						return item.Group;
+					return string.Empty;
+				}
+			});
 		}
 
 	}
@@ -29,20 +32,31 @@ namespace SfListViewTemplateSelector
 		{
 			for (int i = 0; i < 100; i++)
 			{
-				Items.Add(i);
+				Items.Add(new DataItem() { Count = i, Group = Math.Floor((double)(i/10)).ToString()});
 			}
 		}
 
-		public List<int> Items { get; } = new();
+		public List<DataItem> Items { get; } = new();
+	}
+
+	public class DataItem
+	{
+		public int Count { get; set; }
+		public string Group { get; set; }
+
+		public override string ToString()
+		{
+			return Count.ToString();
+		}
 	}
 
 	public class Selector : DataTemplateSelector
 	{
 		protected override DataTemplate OnSelectTemplate(object item, BindableObject container)
 		{
-			var i = (int)item;
-			
-			if (i % 2 == 0)
+			var i = (DataItem)item;
+
+			if (i.Count % 2 == 0)
 			{
 				return new DataTemplate(typeof(Template1));
 			}
@@ -50,6 +64,15 @@ namespace SfListViewTemplateSelector
 			{
 				return new DataTemplate(typeof(Template2));
 			}
+		}
+	}
+
+	public class HeaderSelector : DataTemplateSelector
+	{
+		protected override DataTemplate OnSelectTemplate(object item, BindableObject container)
+		{
+			var g = (GroupResult)item;
+			return new DataTemplate(() => new Label() { Text = g.Key.ToString() });
 		}
 	}
 }
