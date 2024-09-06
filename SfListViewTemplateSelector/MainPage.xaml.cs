721c5df9f99e9f6eca251f8b7e303de17e0bcf50
Brian Macomber <brian@agspectrum.com>
2024-09-06 14:39:08 -0500

    Add project files.

---
+++ SfListViewTemplateSelector/MainPage.xaml.cs
@@ -0,0 +1,35 @@
+﻿namespace SfListViewTemplateSelector
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+
+			BindingContext = new ViewModel();
+		}
+		
+	}
+
+	public class ViewModel
+	{
+
+		public ViewModel()
+		{
+			for (int i = 0; i < 100; i++)
+			{
+				Items.Add(100.ToString());
+			}
+		}
+
+		public List<string> Items { get; } = new();
+	}
+
+	public class Selector : DataTemplateSelector
+	{
+		protected override DataTemplate OnSelectTemplate(object item, BindableObject container)
+		{
+			return new DataTemplate(() => new Label() { Text = "Hi"});
+		}
+	}
+}
